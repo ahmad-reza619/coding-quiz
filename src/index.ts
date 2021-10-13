5902db1c4a95669860e012364209e3c4a9e7a113
Ahmad Reza <hungry.dev39@gmail.com>
2021-10-13 21:10:44 +0700

    reset all & make testing code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express';
-import Discord, { Message } from "discord.js";
-import { DISCORD_TOKEN } from './config/secrets';
-import CommandHandler from './commandHandler';
-import config from './config/botConfig';
+import Discord, { Message, TextChannel  } from "discord.js";
+// import cron from 'node-cron';
+import { DISCORD_TOKEN, QUIZ_TOKEN } from './config/secrets';
+import axios, { AxiosResponse } from 'axios';
 
 const PORT = process.env.PORT || 5000;
 
@@ -18,16 +18,80 @@ app.use('/', (request: Request, response: Response) => {
   response.sendStatus(200);
 });
 
-const commandHandler = new CommandHandler(config.prefix);
-
 //////////////////////////////////////////////////////////////////
 //                    DISCORD CLIENT LISTENERS                  //
 //////////////////////////////////////////////////////////////////
 // Discord Events: https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-channelCreate
 
-client.on("ready", () => { console.log("Hive Greeter has started"); });
-client.on("message", (message: Message) => { commandHandler.handleMessage(message); });
-client.on("error", e => { console.error("Discord client error!", e); });
+client.on("ready", () => { console.log("Coding Quiz Initialized"); });
+
+interface QuizRequest {
+  apiKey: string;
+  limit?: number;
+}
+
+interface QuizResponse {
+  question: string;
+  description: null | string;
+  answers: {
+    answer_a: null | string;
+    answer_b: null | string;
+    answer_c: null | string;
+    answer_d: null | string;
+    answer_e: null | string;
+    answer_f: null | string;
+  },
+  multiple_correct_answers: boolean;
+  correct_answers: {
+    answer_a_correct: boolean;
+    answer_b_correct: boolean;
+    answer_c_correct: boolean;
+    answer_d_correct: boolean;
+    answer_e_correct: boolean;
+    answer_f_correct: boolean;
+  },
+  explanation: string;
+  category: string;
+  difficulity: "Easy" | "Medium" | "Hard";
+}
+
+(async () => {
+  const { data: [question] } = await axios.get<QuizRequest, AxiosResponse<[QuizResponse]>>('https://quizapi.io/api/v1/questions', {
+    params: {
+      apiKey: QUIZ_TOKEN,
+      limit: 1,
+    },
+  });
+  const channel = await client.channels.fetch('897469720381505546') as TextChannel;
+  const answer = Object.entries(question.answers)
+    .reduce((all: string[], current: [string, string | null]) => {
+      if (!current[1]) {
+        return all;
+      }
+      return [
+        ...all,
+        `${current[0].slice(7).toUpperCase()}: ${current[1]}`
+      ]; 
+    }, [])
+  const answerStr = answer.join('\n');
+  const message = await channel
+    .send(
+      `**Quiz Time**\n${question.question}\n\n${answerStr}`,
+    ) as Message;
+  
+  [
+    '🇦',
+    '🇧',
+    '🇨',
+    '🇩',
+    '🇪',
+    '🇫',
+  ].slice(0, answer.length).forEach(reaction => message.react(reaction));
+})()
+
+// cron.schedule('0 21 * * *', async () => {
+// })
 
 client.login(DISCORD_TOKEN);
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
